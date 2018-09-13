1092bf6e7560c8b107a9dc3d68469ca304148720
Emily Stern <emistern@bu.edu>
2018-09-13 14:26:27 -0400

    Initial Commit

---
+++ mini_project_1.py
@@ -0,0 +1,6 @@
+#Copyright Emily Stern, emistern@bu.edu
+
+#This script will download images from a given twitter handle 
+#and then create a video made up of all of the images.
+#It will then use googles video cloud thing to give a 
+#tag to each image.

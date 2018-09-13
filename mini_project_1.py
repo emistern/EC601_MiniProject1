5850f57eabb7342f786c0a9027b93483f18043c8
Emily Stern <emistern@bu.edu>
2018-09-13 16:01:45 -0400

    Added Tweepy Library

--- mini_project_1.py
+++ mini_project_1.py
@@ -4,3 +4,16 @@
 #and then create a video made up of all of the images.
 #It will then use googles video cloud thing to give a 
 #tag to each image.
+
+import tweepy
+from tweepy import OAuthHandler
+ 
+consumer_key = 'YOUR-CONSUMER-KEY'
+consumer_secret = 'YOUR-CONSUMER-SECRET'
+access_token = 'YOUR-ACCESS-TOKEN'
+access_secret = 'YOUR-ACCESS-SECRET'
+ 
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+ 
+api = tweepy.API(auth)

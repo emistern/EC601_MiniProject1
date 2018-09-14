ee053e5e141d60fbe6a6fa31eabf91da1225a246
Emily Stern <emistern@bu.edu>
2018-09-13 21:48:49 -0400

    Ability to download images from url

--- mini_project_1.py
+++ mini_project_1.py
@@ -13,6 +13,9 @@ from tweepy import OAuthHandler
 #Import the twitter credentials
 import twitter_credentials
 
+#Import wget and OS
+import wget, os
+
 #Authorize with the twitter keys/tokens
 auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
@@ -38,6 +41,28 @@ while valid_name == False:
 	    print("That was not a valid username")
 	    continue
 
-new_tweets = api.user_timeline(screen_name = username,count=200)
+#Last 200 tweets from the given username
+new_tweets = api.user_timeline(screen_name = username,count=10)
+
+#Move all of the tweets with images to a new list.
+tweets_with_pics = []
 for tweet in new_tweets:
-	print(tweet)
+	#print(tweet)
+	media = tweet.entities.get('media',[])
+	if (len(media)>0):
+		tweets_with_pics.append(media[0]['media_url'])
+
+#Check if a directory for the pictures exists. If not, make one. 
+try:
+	os.mkdir('twitter_images')
+	os.chdir('twitter_images')
+except:
+	os.chdir('twitter_images')
+
+#Download all of the pictures to the directory twitter_images.
+for pic_url in tweets_with_pics:
+	wget.download(pic_url)
+
+
+
+
